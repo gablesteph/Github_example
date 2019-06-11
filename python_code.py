134faba31c68b4962d2ac45c748c2770ac3376fd
Stephanie Gable <gablesteph@yahoo.com>
2019-06-11 13:29:10 -0400

    adds a python script that prints a message greeting Github

---
+++ python_code.py
@@ -0,0 +1,27 @@
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+python_code.py [unix] (18:59 31/12/1969)                                 0,1 All
+-- INSERT --
+print("Hello Github!")
+
+
